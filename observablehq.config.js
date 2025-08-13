66774060e4851a0d7fe73ab58228e89761c5a665
Akshay Kale <kale@Mac.lan>
2025-08-12 22:55:24 -0500

    update: changed the page layout

--- observablehq.config.js
+++ observablehq.config.js
@@ -6,15 +6,18 @@ export default {
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
+  pages: [
+    {
+      name: "Scared",
+      pages: [
+        {name: "Home", path: "/"},
+        {name: "Network Analysis", path: "/network-analysis"},
+        {name: "Literature Review", path: "/literature-review"},
+        {name: "Individual Report", path: "/individual-report"},
+        {name: "Group Report", path: "/group-report"},
+      ]
+    }
+  ],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
